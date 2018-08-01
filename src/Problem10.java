2323db3173b6045b6ac74f595d6c29ea3b4a209e
Derian123 <derian.escobar@yahoo.com>
2018-08-01 13:18:03 -0400

    Step Problem

---
+++ src/Problem10.java
@@ -0,0 +1,30 @@
+//There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N,
+//write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+public class Problem10 {
+
+    //The logic here is that we know that the base case for no steps and 1 step is 1
+    //We also know that to get the current number of steps we need to add the previous 2 entries to get the current
+    //amount of steps
+    public int numberOfWays(int steps){
+
+        //we make an array to store the entries
+        int[] ways = new int[steps+1];
+        ways[0] = 1;
+        ways[1] = 1;
+
+        //we loop through it adding i-2 + i-1 to the ith position
+        for(int i = 2; i < ways.length;i++){
+
+            ways[i] = ways[i-2] + ways[i-1];
+        }
+        //then we return the number of steps
+        return ways[steps];
+    }
+
+    public static void main(String[] args) {
+        Problem10 p10 = new Problem10();
+        int steps = 4;
+
+        System.out.println(p10.numberOfWays(steps));
+    }
+}
